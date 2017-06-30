11d5e4961ac175cdbc02b03002a1979850de9462
atiqorin <atiqorin@gmail.com>
2017-06-29 19:25:04 -0500

    :bulb: PossibleSums Java

--- Interview/PossibleSums.java
+++ Interview/PossibleSums.java
@@ -1,37 +1,22 @@
 class PossibleSums {
+    HashSet<Integer> set;
+
     int possibleSums(int[] coins, int[] quantity) {
-        int max = 0;
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j < quantity[i]; j++) {
-                max += coins[i];
-            }
+        set = new HashSet<>();
+        solve(coins, quantity, 0, 0);
+
+        return set.size() - 1;
+    }
+
+    void solve(int[] coins, int[] quantity, int index, int currentsum) {
+        if (index == coins.length) {
+            set.add(currentsum);
+            return;
         }
-        int[] arr = new int[coins.length + 1];
-        for (int i = 0; i < coins.length; i++) {
-            arr[i + 1] = coins[i];
+
+        for (int i = 0; i <= quantity[index]; ++i) {
+            solve(coins, quantity, index + 1, currentsum);
+            currentsum += coins[index];
         }
-        int[][] isPossible = new int[coins.length + 1][max + 1];
-        for (int i = 0; i < arr.length; i++) {
-            isPossible[i][0] = 1;
-        }
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j <= max; j++) {
-                for (int k = 0; k <= quantity[i - 1]; k++) {
-                    if (j - k * arr[i] >= 0 && isPossible[i - 1][j - k * arr[i]] == 1) {
-                        isPossible[i][j] = 1;
-                    }
-                }
-            }
-        }
-        int ans = 0;
-        for (int j = 1; j <= max; j++) {
-            for (int i = 0; i < arr.length; i++) {
-                if (isPossible[i][j] == 1) {
-                    ans++;
-                    break;
-                }
-            }
-        }
-        return ans;
     }
 }

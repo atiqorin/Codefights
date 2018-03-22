12589622fed5f6fa88329c52aa13e1750c9794ef
atiqorin <atiqorin@gmail.com>
2018-03-22 13:41:07 -0500

    :bulb: CorrectIPs MYSQL

---
+++ Arcade/Databases/correctIPs.sql
@@ -0,0 +1,9 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE correctIPs()
+BEGIN
+	SELECT id, ip
+    FROM ips
+    WHERE (ip REGEXP '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){2}([1-9][0-9]\.)(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+            OR ip REGEXP '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}([1-9][0-9])$')
+    ORDER BY id;
+END

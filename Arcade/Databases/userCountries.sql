f98a7bd12d828c0a6d15ec45306cb810e73475a0
atiqorin <atiqorin@gmail.com>
2018-03-22 16:35:24 -0500

    :bulb: UserCountries MYSQL

---
+++ Arcade/Databases/userCountries.sql
@@ -0,0 +1,9 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE userCountries()
+BEGIN
+	SELECT u.id, COALESCE(c.country, 'unknown') AS country
+    FROM users AS u
+    LEFT JOIN cities AS c
+    ON u.city = c.city
+    ORDER BY u.id ASC;
+END

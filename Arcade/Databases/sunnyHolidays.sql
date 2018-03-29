a7fd4aa321f64e8e4729656a74dca4e91581cbec
atiqorin <atiqorin@gmail.com>
2018-03-29 11:02:40 -0500

    :bulb: SunnyHolidays MYSQL

---
+++ Arcade/Databases/sunnyHolidays.sql
@@ -0,0 +1,7 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE sunnyHolidays()
+BEGIN
+	SELECT holiday_date AS ski_date
+    FROM holidays
+    INNER JOIN weather ON holiday_date = sunny_date;
+END

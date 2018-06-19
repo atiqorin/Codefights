0f78886236674d074c44a05e5d867cc0b0b0501f
atiqorin <atiqorin@gmail.com>
2018-06-19 13:55:07 -0500

    :bulb: OrderAnalytics MYSQL

---
+++ Arcade/Databases/orderAnalytics.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS orderAnalytics;
+CREATE PROCEDURE orderAnalytics()
+BEGIN
+
+    CREATE OR REPLACE VIEW order_analytics AS
+    SELECT id,
+        year(order_date) AS year,
+        quarter(order_date) AS quarter,
+        type,
+        quantity * price AS total_price
+    FROM orders;
+
+    SELECT *
+    FROM order_analytics
+    ORDER by id;
+END;

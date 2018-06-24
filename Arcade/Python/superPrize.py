43cfa962c3c292bc2a5c8c5a52f5a8c713e5d2b6
atiqorin <atiqorin@gmail.com>
2018-06-24 09:41:59 -0500

    :bulb: SuperPrize Python

---
+++ Arcade/Python/superPrize.py
@@ -0,0 +1,15 @@
+class Prizes(object):
+    def __init__(self, purchases, n, d):
+        self.purchases = purchases
+        self.n = n
+        self.d = d
+        
+    def __iter__(self):
+        for i, x in enumerate(self.purchases):
+            if (i + 1) % self.n == 0 and x % self.d == 0:
+                yield i + 1
+
+
+
+def superPrize(purchases, n, d):
+    return list(Prizes(purchases, n, d))
